6c235f82457b3f04226b5afe39a1802b450c70ec
Nwokedi Chigozirim <nwokedichigozirim747@gmail.com>
2026-04-25 19:00:23 +0100

    Reset config for Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/choir-register',
-  images: {
-    unoptimized: true,
-  },
   reactCompiler: true,
 };
 
